c4ad21552940cc27a0b09e2c8563e0be6a9bea5c
Neylink <146921880+Neylink@users.noreply.github.com>
2023-10-10 11:58:51 +0200

    Add files via upload

---
+++ exo taxi/exo taxi.js
@@ -0,0 +1,31 @@
+
+
+const character  = {
+    nom: "John", 
+    santé: 10,
+};
+const musique = [
+    Anissa,Mettalica,Radwimps,Les Beatles,Les Rolling Stones
+    ];
+const Route = {
+    Feux: 30,
+    Taxis: 0,
+    
+}
+
+{if musique === Anissa; 
+santé = -1;
+else John change Taxis;
+(if santé === 0 alors "John explose");
+}
+
+
+
+
+
+
+
+
+
+
+     
